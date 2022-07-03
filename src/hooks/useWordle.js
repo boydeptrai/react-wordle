6e12cfa376e14986978c6681eb8ea61044077a0f
Tuan Tran <langthamht047@gmail.com>
2022-07-02 20:39:09 -0700

    Coloring Used Keys

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,8 +4,9 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState("");
   const [guesses, setGuesses] = useState([...Array(6)]);
-  const [history, setHistory] = useState(['hello','ninja']);
+  const [history, setHistory] = useState([]);
   const [isCorrect, setIsCorrect] = useState(false);
+  const [usedKeys,setUsedKeys] = useState({})
 
   // format a guess into an array of letter  objects
   // e.g [{key:'a',color:'yellow'}]
@@ -51,6 +52,28 @@ const useWordle = (solution) => {
     setTurn((prevTurn) =>{
       return prevTurn + 1
     })
+
+    setUsedKeys((prevUsedKeys) =>{
+      let newKeys = {...prevUsedKeys}
+
+      formattedGuess.forEach((l) =>{
+        const currentColor =newKeys[l.key]
+
+        if (l.color === 'green') {
+          newKeys[l.key] = 'green'
+          return
+        }
+        if (l.color === 'yellow' && currentColor !== 'green') {
+          newKeys[l.key] = 'yellow'
+          return
+        }
+        if (l.color === 'grey' && currentColor !== 'green' && currentColor !== 'yellow') {
+          newKeys[l.key] = 'grey'
+          return
+        }
+      })
+      return newKeys
+    })
     setCurrentGuess('')
   };
 
@@ -89,6 +112,6 @@ const useWordle = (solution) => {
     }
   };
 
-  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+  return { turn, currentGuess, guesses, usedKeys ,isCorrect, handleKeyup };
 };
 export default useWordle;
